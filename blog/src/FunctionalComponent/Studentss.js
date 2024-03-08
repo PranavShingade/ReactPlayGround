7cd8c067fd5e08e6080ecaba8f13262d3b905e42
67124_Pranav <shingadepranav8@gmail.com>
2024-03-09 01:40:41 +0530

    Access Object Of Array

--- blog/src/FunctionalComponent/Studentss.js
+++ blog/src/FunctionalComponent/Studentss.js
@@ -1,16 +1,37 @@
 import React from 'react'
-
+import '../FunctionalComponent/style.css'
 function Studentss(){
 
-    const student = ['Pranav','Amey','Shubham','Abhi','Amit'];
+    const student = [
+        {Name:"Pranav",Email:"Pranav@gmail.com",Contact:123},
+        {Name:"Amey",Email:"Amey@gmail.com",Contact:321},
+        {Name:"Abhi",Email:"Abhi@gmail.com",Contact:456},
+        {Name:"Amit",Email:"Amit@gmail.com",Contact:654},
+        {Name:"Shubh",Email:"Shubh@gmail.com",Contact:789},
+     ];
     return(
-     <div>
-        <h2>Handle Array with List</h2>
-        {
+     <div className='asb'>
+        <div>
+            <h2>Handle Array with list..</h2><br/><br/>
+            <table border="1">
+                <tr> 
+                    <td>NAME</td>
+                    <td>EMAIL</td>
+                    <td>CONTACT</td>
+                </tr>
+        
+         {
             student.map((data)=>
-                <h3>Name : {data}</h3>
+                <tr>
+                    <td>{data.Name}</td>
+                    <td>{data.Email}</td>
+                    <td>{data.Contact}</td>
+                </tr>
             )
-        }
+         }
+            </table>
+        </div>
+        
     </div>
     )
 }
