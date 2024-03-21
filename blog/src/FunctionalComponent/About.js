e71d61ba5aa729a0fcae7591ce66c542790a88d7
67124_Pranav <shingadepranav8@gmail.com>
2024-03-21 22:34:21 +0530

    Routes,Links

--- blog/src/FunctionalComponent/About.js
+++ blog/src/FunctionalComponent/About.js
@@ -3,6 +3,8 @@ function About(){
     return(
         <>
         <h1>About Page</h1>
+        <h3>This is about page of our awesome app </h3>
+        <h3> & here we learning about React Router</h3>
         </>
     )
 }
---
+++ blog/src/FunctionalComponent/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import About from "./About"
+import Home from './Home'
+import Boot from './Table'
+function Navbar(){
+    return(
+        <>
+        <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/About'>About us</Link></li>
+            <li><Link to='/boot'>Users</Link></li>
+        </ul>
+        </>
+    )
+}
+export default Navbar
