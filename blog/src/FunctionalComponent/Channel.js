87ad99e5b0e0758c0390d082bf4426aa5c9f89d9
67124_Pranav <shingadepranav8@gmail.com>
2024-03-29 23:05:55 +0530

    nested Routing

---
+++ blog/src/FunctionalComponent/Channel.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Channel(){
+    return(
+        <div>
+            <h2>YouTube Channel Page</h2>
+            <h2>Hotstar Channel Page</h2>
+            <h2>Zee5 Channel Page</h2>
+        </div>
+    )
+}
+export default Channel
